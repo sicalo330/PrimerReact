29882ee54f72ae9864c5dfa9ae8363a213116600
sicalo330 <132423242+sicalo330@users.noreply.github.com>
2023-10-20 11:02:02 -0500

    Acabar el proyecto

--- src/App.js
+++ src/App.js
@@ -6,7 +6,27 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>Estos es lo que dicen nuestros alumnos sobre react:</h1>
-        <Testimonio/>
+        <Testimonio
+        nombre="Shawn Wang"
+        pais="Singapore"
+        imagen="Shawn"
+        cargo="Ingeniero de software"
+        empresa="Amazon"
+        testimonio="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life" />
+        <Testimonio
+        nombre="Sarah Chima"
+        pais="Nigeria"
+        imagen="Sarah"
+        cargo="Ingeniera de software"
+        empresa="ChatDesk"
+        testimonio="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company." />
+        <Testimonio
+        nombre = "Emma Bostian"
+        pais="Suecia"
+        imagen="Emma"
+        cargo="Ingeniera de software"
+        empresa="Spotify"
+        testimonio="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify" />
       </div>
     </div>
   );
--- src/componentes/testimonio.js
+++ src/componentes/testimonio.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import '../hojasEstilo/testimonio.css'
 
-function Testimonio(){
+function Testimonio(props){ //Esto es para volver más dinámico el texto estático de react
   return(
     <div className='contenedor-testimonio'>
       <img
       className='imagen-testimonio'
-      src={require('../imagenes/Emma.png')} alt='Imagen'/>
+      src={require(`../imagenes/${props.imagen}.png`)} alt={props.nombre}/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Ema Bostian en Suecia</p>
-        <p className='Cargo-testimonio'>Ingeniería de software spotify</p>
-        <p className='texto-testimonio'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+        <p className='nombre-testimonio'>
+         <strong>{props.nombre}</strong> en {props.pais} </p>
+        <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
     </div>
   );
