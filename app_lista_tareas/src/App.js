19cacc4f44a0ef64c546cb8e37ff6b43d311be7e
sicalo330 <132423242+sicalo330@users.noreply.github.com>
2023-12-07 19:10:52 -0500

    Crear lógica para agregar tareas

--- app_lista_tareas/src/App.js
+++ app_lista_tareas/src/App.js
@@ -1,10 +1,27 @@
+import React,{useState} from 'react';
 import './App.css';
 import Header from './componentes/Header.js';
+import FormularioTareas from './componentes/FormularioTareas.js';
 
 const App = () => {
+  const [tareas,cambiarTareas] = useState(
+    [
+      {
+        id:1,
+        texto:"Lavar la ropa",
+        completada: false
+      },
+      {
+        id:2,
+        texto:"Grabar tutorial",
+        completada: false
+      }
+    ]
+    );
   return (
     <div className='contenedor'>
       <Header />
+      <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas} />
     </div>
   );
 }
---
+++ app_lista_tareas/src/componentes/FormularioTareas.js
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import {v4 as uuid4} from 'uuid';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+
+const FormularioTreas = ({tareas, cambiarTareas}) => {
+    //Con esto capturamos el valor del input
+    const [inputTarea, cambiarInputTarea] = useState('')
+
+    const handleInput = (e) => {
+        cambiarInputTarea(e.target.value)
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        cambiarTareas([
+            ...tareas,//los tres puntos se llamann desestructuración de arrays y permite agregar un array de la variable que está al lado
+            {
+                id:uuid4(),//Para hacer la id dinámica usé npm install uuid en la carpeta de este proyecto
+                texto:inputTarea,
+                completada:false
+            }
+        ]);//Al parecer
+    }
+    return(
+        <form action="" className="formulario-tareas" onSubmit={handleSubmit}>
+            <input
+            type="text"
+            className="formulario-tareas__input"
+            placeholder="Escribe una tarea"
+            value={inputTarea}
+            onChange={(e) => handleInput(e)}
+            />
+            <button
+            type="submit"
+            className="formularios-tareas__btn"
+            >
+                <FontAwesomeIcon
+                icon={faPlusSquare}
+                className="formulario-tareas__icono-btn" />
+            </button>
+        </form>
+    );
+}
+
+export default FormularioTreas;
