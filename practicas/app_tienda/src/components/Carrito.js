5788ef11c1469023b47a182746259eda5f080c5d
sicalo330 <132423242+sicalo330@users.noreply.github.com>
2023-12-27 15:50:56 -0500

    Hacer simulador de tienda

---
+++ practicas/app_tienda/src/components/Carrito.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+const Carrito = ({carrito}) => {
+
+    return(
+        <div>
+            <h3>Carrito de compras</h3>
+            {carrito.length > 0 ?
+            carrito.map((producto, index) => {
+                return(
+                    <Producto key={index}>
+                        <NombreProducto>
+                            {producto.nombre}
+                        </NombreProducto>
+                            Cantidad: {producto.cantidad}
+                    </Producto>
+                );
+            })
+            :
+            <p>Aún no hay productos al carrito</p>
+            }
+        </div>
+    );
+}
+
+const Producto = styled.div`
+    padding: 10px;
+    border-bottom:1px solid #ebebf3;
+    font-size:14px;
+`;
+
+const NombreProducto = styled.p`
+    font-weight:bold;
+    font-size:16px;
+    color:#000;
+`
+
+export default Carrito;
